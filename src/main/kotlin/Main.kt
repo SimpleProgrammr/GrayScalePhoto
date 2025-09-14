6ad231400d681c17875638e3a1c3b7256d3e1a37
Programmr <michal.pelon@proton.me>
2025-09-14 16:09:11 +0200

    Refactor Main.kt and update Maven build plugins
    Encapsulated main logic in a Main class in Main.kt and improved code formatting. Updated pom.xml to add Spring Boot Maven plugin and configure Kotlin plugin to build executable JARs. Minor cleanup in .idea/misc.xml.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,55 +3,56 @@ package space.badluck
 import java.io.File
 import java.util.stream.IntStream
 import javax.imageio.ImageIO
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+class Main {
+    //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main(args: Array<String>) {
-    if(args.isEmpty())
-    {
-        println("No input image provided")
-        return
-    }
-    if(args[0]=="-h")
-    {
-        println("For help look to repository :P\n" +
-                "Just use pth to your image as parameter.")
-        return
-    }
-    val file = File(args[0])
-    if(!file.exists()){
-        println("Error 404. File do NOT exist")
-        return
-    }
-
-val supportedFileExtensions = arrayOf(
-    "png", "jpg","tiff","gif","bmp"
-    )
-
-    if(!supportedFileExtensions.contains(file.extension)){
-        println("Unsupported extension")
-        return
-    }
-
-    val image = ImageIO.read(file)
-    val height = image.height
-    val width = image.width
-
-    IntStream.range(0,height).parallel().forEach { y ->
-        IntStream.range(0,width).parallel().forEach { x ->
-            val pixel = image.getRGB(x,y) and 0x00FFFFFF
-
-            val R = (pixel and 0xff0000)/0xffff
-            val G = (pixel and 0x00ff00)/0xff
-            val B = (pixel and 0x0000ff)
-            val avg = ((R+G+B)/3).toHexString().substring(6)
-
-            val avgColor: Int =  ("FF" + avg + avg + avg).toLong(16).toInt()
-
-            image.setRGB(x,y,avgColor)
+    fun main(args: Array<String>) {
+        if (args.isEmpty()) {
+            println("No input image provided")
+            return
         }
-    }
-    val outputFile = File("${file.path.replace("."+file.extension,"")}-GrayScale.${file.extension}")
-    ImageIO.write(image,file.extension,outputFile)
+        if (args[0] == "-h") {
+            println(
+                "For help look to repository :P\n" +
+                        "Just use pth to your image as parameter."
+            )
+            return
+        }
+        val file = File(args[0])
+        if (!file.exists()) {
+            println("Error 404. File do NOT exist")
+            return
+        }
 
+        val supportedFileExtensions = arrayOf(
+            "png", "jpg", "tiff", "gif", "bmp"
+        )
+
+        if (!supportedFileExtensions.contains(file.extension)) {
+            println("Unsupported extension")
+            return
+        }
+
+        val image = ImageIO.read(file)
+        val height = image.height
+        val width = image.width
+
+        IntStream.range(0, height).parallel().forEach { y ->
+            IntStream.range(0, width).parallel().forEach { x ->
+                val pixel = image.getRGB(x, y) and 0x00FFFFFF
+
+                val R = (pixel and 0xff0000) / 0xffff
+                val G = (pixel and 0x00ff00) / 0xff
+                val B = (pixel and 0x0000ff)
+                val avg = ((R + G + B) / 3).toHexString().substring(6)
+
+                val avgColor: Int = ("FF" + avg + avg + avg).toLong(16).toInt()
+
+                image.setRGB(x, y, avgColor)
+            }
+        }
+        val outputFile = File("${file.path.replace("." + file.extension, "")}-GrayScale.${file.extension}")
+        ImageIO.write(image, file.extension, outputFile)
+
+    }
 }
